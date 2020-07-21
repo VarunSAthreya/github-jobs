65c55d2f47cf10e8f432c1fee99c9403f03991ff
Varun S Athreya <varun.sathreya@gmail.com>
2020-07-21 21:08:41 +0530

    fixed cancelToken error

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -32,12 +32,11 @@ const reducer = (state, action) => {
 };
 
 export default function useFetchJobs(params, page) {
-    const cancelToken1 = axios.CancelToken.source();
-    const cancelToken2 = axios.CancelToken.source();
-
     const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true });
 
     useEffect(() => {
+        const cancelToken1 = axios.CancelToken.source();
+        const cancelToken2 = axios.CancelToken.source();
         dispatch({ type: ACTIONS.MAKE_REQUEST });
 
         axios
@@ -67,7 +66,7 @@ export default function useFetchJobs(params, page) {
             .then((res) => {
                 dispatch({
                     type: ACTIONS.UPDATE_HAS_NEXT_PAGE,
-                    payload: { hasNextPage: res.data.lenght !== 0 },
+                    payload: { hasNextPage: res.data.length !== 0 },
                 });
             })
             .catch((e) => {
