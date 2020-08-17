d73826744d24f52bc706613acdd8f9257c26e596
Varun S Athreya <varun.sathreya@gmail.com>
2020-08-18 04:50:54 +0530

    Made form auto size

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -5,25 +5,29 @@ function SearchForm({ params, onParamChange }) {
     return (
         <Form className="mb-4">
             <Form.Row className="align-items-end">
-                <Form.Group as={Col}>
-                    <Form.Label>Description</Form.Label>
-                    <Form.Control
-                        onChange={onParamChange}
-                        value={params.description}
-                        name="description"
-                        type="text"
-                    />
-                </Form.Group>
+                <Col xs="auto">
+                    <Form.Group as={Col}>
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control
+                            onChange={onParamChange}
+                            value={params.description}
+                            name="description"
+                            type="text"
+                        />
+                    </Form.Group>
+                </Col>
 
-                <Form.Group as={Col}>
-                    <Form.Label>Loaction</Form.Label>
-                    <Form.Control
-                        onChange={onParamChange}
-                        value={params.loaction}
-                        name="loaction"
-                        type="text"
-                    />
-                </Form.Group>
+                <Col xs="auto">
+                    <Form.Group as={Col}>
+                        <Form.Label>Loaction</Form.Label>
+                        <Form.Control
+                            onChange={onParamChange}
+                            value={params.loaction}
+                            name="loaction"
+                            type="text"
+                        />
+                    </Form.Group>
+                </Col>
 
                 <Form.Group as={Col} xs="auto" className="ml-2">
                     <Form.Check
